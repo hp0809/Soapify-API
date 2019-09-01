d4219af92ea35f25e036490dc96175a5cb0945a7
Hali Power <hpower0809@gmail.com>
2019-09-01 14:30:14 +0000

    update

--- migrations/002.do.create_soapify_users.sql
+++ migrations/002.do.create_soapify_users.sql
@@ -6,8 +6,3 @@ CREATE TABLE soapify_users (
   email TEXT NOT NULL,
   date_created TIMESTAMP NOT NULL DEFAULT now()
 );
-
-ALTER TABLE user_soaps
-  ADD COLUMN
-    user_id INTEGER REFERENCES soapify_users(id)
-    ON DELETE SET NULL;
--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://hp0809:hp0809@localhost/soapifydb',
-    JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
+    DB_URL: process.env.DATABASE_URL  || 'postgresql://hp0809:hp0809@localhost/soapifydb',
+    JWT_SECRET: process.env.JWT_SECRET || process.env.JWT_SECRET,
     JWT_EXPIRY: process.env.JWT_EXPIRY || '3h'
 }
